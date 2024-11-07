27b2399b87d8910f880fa498206024172e1d11db
nirshtam <nirshtam@gmail.com>
2024-11-07 14:47:54 +0100

    Made Header Sticky

--- frontend/src/components/HeaderMenu.tsx
+++ frontend/src/components/HeaderMenu.tsx
@@ -2,25 +2,25 @@
 import React from 'react';
 import { Menu, Layout } from 'antd';
 import { NavLink } from 'react-router-dom';
-
+import '../css/Header.css';
 const { Header } = Layout;
 
 const HeaderMenu: React.FC = () => {
-  return (
-    <Header>
-      <Menu theme="dark" mode="horizontal">
-        <Menu.Item key="1">
-          <NavLink to="/">Page 1</NavLink>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <NavLink to="/page2">Page 2</NavLink>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <NavLink to="/page3">Page 3</NavLink>
-        </Menu.Item>
-      </Menu>
-    </Header>
-  );
+    return (
+        <Header>
+            <Menu theme="dark" mode="horizontal">
+                <Menu.Item key="1">
+                    <NavLink to="/">Page 1</NavLink>
+                </Menu.Item>
+                <Menu.Item key="2">
+                    <NavLink to="/page2">Page 2</NavLink>
+                </Menu.Item>
+                <Menu.Item key="3">
+                    <NavLink to="/page3">Page 3</NavLink>
+                </Menu.Item>
+            </Menu>
+        </Header>
+    );
 };
 
 export default HeaderMenu;
